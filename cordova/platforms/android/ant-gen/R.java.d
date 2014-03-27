cfa38d63338209925b4d1a057fb09efc29a4f071
Gen Liu <gerald_721@hotmail.com>
2014-03-27 17:51:01 +0800

    change default theme

--- cordova/platforms/android/ant-gen/R.java.d
+++ cordova/platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@
 /home/gg/cuttingedge/cordova/platforms/android/res/drawable/icon.png \
 /home/gg/cuttingedge/cordova/platforms/android/res/xml/config.xml \
 /home/gg/cuttingedge/cordova/platforms/android/res/drawable-hdpi/icon.png \
+/home/gg/cuttingedge/cordova/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/home/gg/cuttingedge/cordova/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/home/gg/cuttingedge/cordova/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/gg/cuttingedge/cordova/platforms/android/ant-build/res/drawable/icon.png \
+/home/gg/cuttingedge/cordova/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/gg/cuttingedge/cordova/platforms/android/ant-build/AndroidManifest.xml \
